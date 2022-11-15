89451b488974304820e30969dd7e463837406caf
Carlos Macarao <82442020+carlosmacarao@users.noreply.github.com>
2022-11-15 21:20:34 +0200

    primeiros programas java

---
+++ swing/JFrame.java
@@ -0,0 +1,16 @@
+public class JFrame {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(399, 200);
+        frame.setVisible(true);
+    }
+
+    private void setVisible(boolean b) {
+    }
+
+    private void setSize(int w, int h) {
+    }
+}
